e0448dfc1c91352f2399aa85afa6b3b352f5b442
Jiang Xilun <jiangxilun@foxmail.com>
2018-02-05 22:56:20 +0800

    排序算法更新

--- sort.py
+++ sort.py
@@ -1,95 +1,68 @@
-nums = [5, 4, 3, 7, 8, 10, 9, 5, 3, 4]
+# coding=utf-8
+nums = [1, 4, 7, 1, 5, 5, 3, 85, 34, 75, 23, 75, 2, 0]
 
 
-def maopao(nums):
-    length = len(nums)
+def maopao(arr):
+    length = len(arr)
     for i in range(length - 1):
         for j in range(length - i - 1):
-            if nums[j] > nums[j + 1]:
-                temp = nums[j]
-                nums[j] = nums[j + 1]
-                nums[j + 1] = temp
-    return nums
+            if arr[j] > arr[j + 1]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
-def xuanze(nums):
-    length = len(nums)
+def xuanze(arr):
+    length = len(arr)
     for i in range(length - 1):
         for j in range(i + 1, length):
-            if nums[j] < nums[i]:
-                temp = nums[j]
-                nums[j] = nums[i]
-                nums[i] = temp
-    return nums
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
-# def partition(nums, left, right):
-#     # if left == right:
-#     #     return left
-#     j = right
-#     i = left+1
-#     while i < j:
-#         while i < j and nums[j] >= nums[left]:
-#             j -= 1
-#         while i < j and nums[i] <= nums[left]:
-#             i += 1
-#         temp = nums[i]
-#         nums[i] = nums[j]
-#         nums[j] = temp
-#     if i != left+1 and j != right:
-#         temp = nums[left]
-#         nums[left] = nums[j]
-#         nums[j] = temp
-#         return j
-#     elif j == right:
-#         temp = nums[left]
-#         nums[left] = nums[right]
-#         nums[right] = temp
-#         return right
-#     else: return j-1
+# 快排
 
-def parttion(v, left, right):
-    key = v[left]
-    low = left
-    high = right
-    while low < high:
-        while (low < high) and (v[high] >= key):
-            high -= 1
-        v[low] = v[high]
-        while (low < high) and (v[low] <= key):
-            low += 1
-        v[high] = v[low]
-        v[low] = key
-    return low
+def quicksort(arr, head=0, tail=0):
+    if head < tail - 1:
+        middle = partition(arr, head, tail)
+        quicksort(arr, head, middle)
+        quicksort(arr, middle + 1, tail)
+    else:
+        return
 
-def quicksort(v, left, right):
-    if left < right:
-        p = parttion(v, left, right)
-        quicksort(v, left, p-1)
-        quicksort(v, p+1, right)
-    return v
 
-# def quicksort(nums, left, right):
-#     middle = partition(nums, left, right)
-#     print nums
-#     if left<middle-1:
-#         quicksort(nums, left, middle-1)
-#     if right>middle+1:
-#         quicksort(nums, middle+1, right)
-#     return
+def partition(arr, head, tail):
+    i = head - 1
+    for j in range(head, tail - 1):
+        if arr[j] < arr[tail - 1]:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+    # 经历过上步的循环后，i+1指向的一定是比tail-1指向的数大的数，所以下一步可以和tail-1放心交换
+    arr[i + 1], arr[tail - 1] = arr[tail - 1], arr[i + 1]
+    return i + 1
+    # 返回的是middle的位置，这个位置已经排好序了，所以下一步的partition不应该取到这个位置，
+    # 下个partition只应该管到middle-1的位置
 
-def kuaisu(nums):
-    length = len(nums)
-    return quicksort(nums, 0, length - 1)
 
-def charu(nums):
-    length=len(nums)
-    if length>1:
-        
+# 插入排序
+def insert_sort(arr):
+    length = len(arr)
+    for i in range(1, length):
+        key = arr[i]
+        j = i - 1
+        while j >= 0:
+            if arr[j] > key:
+                arr[j + 1] = arr[j]
+                arr[j] = key
+            j -= 1
+    return
 
-#print nums
-#print maopao(nums[:])
-#print nums
-#print xuanze(nums[:])
+
+# 希尔排序
+
+# 归并排序
+nums2 = nums[:]
+insert_sort(nums2)
+# quicksort(nums2, tail=len(nums))
 print nums
-print kuaisu(nums[:])
+print nums2
